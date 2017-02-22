52aa900d8fa891bc05d99197a983517923f4449a
Gerardo Burkle <burkle8@gmail.com>
2017-02-22 17:49:45 -0600

    Big updates. You can now stalk prey

--- Stalker.py
+++ Stalker.py
@@ -2,8 +2,8 @@
 import sys, os
 #import json
 #from  Intel_Feeds import open_source_lists as feeds
-from Database import updateDatabase as dbupdate
-
+from Database import database as dbupdate
+from Stalk import stalk_prey as stalk
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
 ################################################################################################################################
@@ -19,10 +19,9 @@ def main():
 			cls()
 			dbupdate.dbMenu()
 		elif choise == 2:
-			print (choise)
-			input()
+			stalk.spMenu()	
 		elif choise == 3:
-			print (choise)
+			print ("This has not been implemented.")
 			input()
 		else:
 			print ("Invalid number. Get some coffee and try again...")
@@ -62,9 +61,9 @@ ________________________________________________________________________________
  `Y8888P ,88P'       8 8888       .8'       `8. `88888. 8 888888888888 8 8888     `Y8. 8 888888888888 8 8888     `88.
 ________________________________________________________________________________________________________________________
 
-[1] Update Database.
-[2] option 2
-[3] option 3
+[1] Database.
+[2] Stalk. 
+[3] Modules.
 
 [4] Quit
 
