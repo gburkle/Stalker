89a3ad13ec3445c50da2d12edcc7fced97e5d94f
Gerardo Burkle <burkle8@gmail.com>
2017-02-22 23:46:59 -0600

    Mayor Updates

--- Stalk/stalk_prey.py
+++ Stalk/stalk_prey.py
@@ -3,7 +3,7 @@ Created on Feb 22, 2017
 
 @author: tesoro
 '''
-import os
+import os,re
 from Database import connections as dbconnect
 
 def spMenu():
@@ -15,6 +15,7 @@ def spMenu():
 #        MD5 Hash           #
 #       Domain Name         #
 #        IP Address         #
+#         email             #
 #                           #
 #############################
 
@@ -40,14 +41,68 @@ def spMenu():
 
 def stalkPrey():
     opensourcedb = dbconnect.opensourcelistsColl()
-    #etpdb = dbconnect.feEtpColl()
+    etpdb = dbconnect.feEtpColl()
+    emailPattern = re.compile(r'[^@]+@[^@]+\.[^@]+')
+    
     try:
         print ("\n")
         prey = input ("Input Prey: ")
-        if opensourcedb.find({'indicator':prey}).count() > 0:
+        
+        if re.match(emailPattern, prey):
+            try:
+                if etpdb.find({'from':prey}).count() == 1:
+                    #print("Prey is an email address and it's on the database")
+                    results = etpdb.find({'from':prey})
+                    print ("IntelSource : FireEye ETP")
+                    print ("FireETP Alert Number: ", results[0]['alert'])
+                    print ("Alert date: ", results[0]['time'])
+                    print ("Alert type: ", results[0]['type'])
+                    print ("File name or URL: ", results[0]['name'])
+                    print ("File Hash:", results[0]['md5'])
+                    print ("Sent from: ", results[0]['from'])
+                    print ("Sent to: ", results[0]['recipients'])
+                    print ("Email Subject: ", results[0]['subject'])
+                    print ("Notes or related IoCs: ", str(results[0]['evilips']))
+                    
+                elif etpdb.find({'from':prey}).count() > 1:
+                    print (etpdb.find({'from':prey}).count(), " preys were found!!\n")
+                    choise = input ("Do you want to stalk them all? [Y/N]: ")
+                    if choise == 'Y' or 'y':
+                        results = etpdb.find({'from':prey})
+                        for hit in results:
+                            print(hit)
+                            print("\n")
+                    
+                        
+            except Exception as e: print (e)
+        
+        
+        
+        elif opensourcedb.find({'indicator':prey}).count() > 0:
+            
             results = opensourcedb.find({'indicator':prey})
-            for hit in results:
-                print(hit)
+            
+            print ("Prey found, stalking...\n")
+            #print (results[0]['intelsource'])
+            if results[0]['intelsource'] == 'FireEye_ETP':
+                etpalertnumber = results[0]['notes'][0]['alert']
+                results = etpdb.find({'alert':etpalertnumber})
+                print ("IntelSource : FireEye ETP")
+                print ("FireETP Alert Number: ", results[0]['alert'])
+                print ("Alert date: ", results[0]['time'])
+                print ("Alert type: ", results[0]['type'])
+                print ("File name or URL: ", results[0]['name'])
+                print ("File Hash:", results[0]['md5'])
+                print ("Sent from: ", results[0]['from'])
+                print ("Sent to: ", results[0]['recipients'])
+                print ("Email Subject: ", results[0]['subject'])
+                print ("Notes or related IoCs: ", str(results[0]['evilips']))
+            else:
+                #for hit in results:
+                #    print(hit)
+                print ("IntelSource: ", results[0]['intelsource'])
+                print ("Type: ", results[0]['type'])
+                print ("Date added to StalkerDB: ", results[0]['date'])
         else:
             print("\n")
             print ("No prey was found\n")
